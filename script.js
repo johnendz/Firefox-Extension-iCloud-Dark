abe2d8f16106ec6f42ab485634dd4ee174c489d9
Alexander Friedrich <johnendzsocial@icloud.com>
2020-02-22 23:09:50 -0300

    Initial commit

--- script.js
+++
@@ -1,6 +0,0 @@
-// mudar imagem da tela inicial | change image home
-$('iframe').load( function() {
-    var	contents = $('iframe').contents(),
-		body = contents.find('body'),
-		styleTag = $('<style>hole</style>').appendTo(contents.find('head'));
-});
